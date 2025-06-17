da6999b42fc945932abe8d8c19e7d9aa887d8d10
Hoàng Bình <hpbinh200x@gmail.com>
2025-06-17 23:25:30 +0700

    dựng base, cấu hình router/layout

---
+++ src/routes/index.js
@@ -0,0 +1,22 @@
+// Layouts
+import { HeaderOnly } from '~/components/Layout';
+
+import HomePage from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+
+// Route không cần đăng nhập vẫn vào được
+const publicRoutes = [
+    { path: '/', component: HomePage },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+];
+
+// Route đăng nhập mới vào được
+const privateRoutes = [];
+
+export { privateRoutes, publicRoutes };
